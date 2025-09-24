0f48e2a503faa36c0be20773007832649bbe98ca
Thaquidheen M A <71427234+Thaquidheen@users.noreply.github.com>
2025-09-24 19:36:33 +0530

    Delete erp-backend/target directory

--- erp-backend/target/classes/db/migration/V2__Add_active_column_to_user_bank_details.sql
+++
@@ -1,5 +0,0 @@
--- V2__Add_active_column_to_user_bank_details.sql
--- Add missing active column to user_bank_details table
-
-ALTER TABLE user_bank_details
-ADD COLUMN active BOOLEAN NOT NULL DEFAULT true;
